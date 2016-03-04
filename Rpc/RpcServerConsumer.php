cc948916ffe942e7b6276fd74eaf1d97d99918ca
Daniel Oliveira <daniel@headdev.com.br>
2016-03-04 10:05:49 -0300

    improve rpc service consumer

---
+++ Rpc/RpcServerConsumer.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Cmobi\RabbitmqBundle\Rpc;
+
+/**
+ * Cosumer to call another rpc server given current server
+ *
+ * Class RpcServerConsumer
+ * @package Cmobi\RabbitmqBundle\Rpc
+ */
+class RpcServerConsumer extends RpcClient
+{
+    public function call()
+    {
+        $this->refreshChannel();
+        parent::call();
+    }
+}
